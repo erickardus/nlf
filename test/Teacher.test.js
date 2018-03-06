a5c71c647d1ee30b47192b2ff5c7fe72ba18b1cb
Erick Ramirez <erick.rmz@samsung.com>
2018-03-06 14:44:55 -0800

    Improve tests

---
+++ test/Teacher.test.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let teacher;
+
+const INITIAL_STRING = "Hi there!";
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  
+  // Use one of those accounts to deploy the contract
+  teacher = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' })
+
+  teacher.setProvider(provider);
+
+});
+
+describe('Teacher', () => {
+  it('deploys a contract', () => {
+    assert.ok(teacher.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await teacher.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => {
+    await teacher.methods.setMessage('hey').send({ from: accounts[0] });
+    const message = await teacher.methods.message().call();
+    assert.equal(message, 'hey');
+  });
+
+
+});
